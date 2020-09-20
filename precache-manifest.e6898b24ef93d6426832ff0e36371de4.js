a860ade557e12fb3b7db5a920144b0cac61a5fed
ninjapj25 <ninjapj25@gmail.com>
2020-09-20 20:25:24 +0800

    Updates

---
+++ precache-manifest.e6898b24ef93d6426832ff0e36371de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18e30407290e9efe508545bf3b6f02c",
+    "url": "/TodoReactApp/index.html"
+  },
+  {
+    "revision": "d7d7eb5d32750df7a85d",
+    "url": "/TodoReactApp/static/css/main.980f8858.chunk.css"
+  },
+  {
+    "revision": "d35e77339eb85d484b2a",
+    "url": "/TodoReactApp/static/js/2.eb292f3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoReactApp/static/js/2.eb292f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d7eb5d32750df7a85d",
+    "url": "/TodoReactApp/static/js/main.5644b997.chunk.js"
+  },
+  {
+    "revision": "0bb0e076be61e329cf12",
+    "url": "/TodoReactApp/static/js/runtime-main.c91ea794.js"
+  }
+]);
